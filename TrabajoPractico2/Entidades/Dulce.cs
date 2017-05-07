f46b199c1c0c46925eb5aa3c64f493f9288c7db0
guidoves <guido.vespignani@gmail.com>
2017-05-07 18:29:53 -0300

    Trabajo Practico 2

---
+++ TrabajoPractico2/Entidades/Dulce.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    public class Dulce : Producto
+    {
+        public Dulce(EMarca marca, string codigoDeBarras, ConsoleColor color)
+        {
+            base._codigoDeBarras = codigoDeBarras;
+            base._colorPrimarioEmpaque = color;
+            base._marca = marca;
+        }
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 80;
+            }
+            
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DULCE");
+            sb.AppendLine((string)this);
+            sb.AppendLine("CALORIAS : " + this.CantidadCalorias);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
